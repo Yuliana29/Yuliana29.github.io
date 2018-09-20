84b4e6525a46c1ee2678df90d2cf87c0d0851a35
Yuliana29 <39260172+Yuliana29@users.noreply.github.com>
2018-09-20 11:45:26 +0300

    Add files via upload

---
+++ script2.js
@@ -0,0 +1,34 @@
+﻿function $()
+{ var elements = new Array();
+for (var i = 0; i < arguments.length; i++)
+{ var element = arguments[i];
+if (typeof element == 'string')
+element = document.getElementById(element);
+if (arguments.length == 1)
+return element;
+elements.push(element);
+}
+return elements; }
+
+function f1(){
+$('x2').style.color='green';
+}
+
+function f2(){
+$('x2').style.color='red';
+}
+function f3(){
+$('x2').style.color='blue';
+}
+function f4(){
+$('x2').style.color='purple';
+}
+function f5(){
+
+$('tx1').value="Привет,мир!";
+alert("Привет,мир!");
+
+}
+
+
+
