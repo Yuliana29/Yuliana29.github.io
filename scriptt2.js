6b09bc9fbb4dcab49535f9b466e16a6a7f0f1ed6
Yuliana29 <39260172+Yuliana29@users.noreply.github.com>
2018-09-25 20:53:08 +0300

    Add files via upload

---
+++ scriptt2.js
@@ -0,0 +1,34 @@
+﻿function $()
+{ var elements = new Array();
+for (var i = 0; i < arguments.length; i++)
+{ var element = arguments[i];
+if (typeof element == 'string')
+element = document.getElementById(element);
+if (arguments.length == 1)
+return element;
+elements.push(element);
+}
+return elements; }
+
+function f6(){
+$('p1').src="буба.png";
+}
+
+function f7(){
+$('p1').src="чирва.png";
+}
+function f8(){
+$('p1').src="пика.png";
+}
+function f9(){
+$('p1').src="трефа.png";
+}
+function f10(){
+
+$('tx1').value="Привет,мир!";
+alert("Привет,мир!");
+
+}
+
+
+
