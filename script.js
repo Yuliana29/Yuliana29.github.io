8807e41ba16f7e23fe3502055aff6441599fec24
Yuliana29 <39260172+Yuliana29@users.noreply.github.com>
2018-09-27 12:38:19 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+﻿function $()
+{ var elements = new Array();
+for (var i = 0; i < arguments.length; i++)
+{ var element = arguments[i];
+if (typeof element == 'string')
+element = document.getElementById(element);
+if (arguments.length == 1)
+return element;
+elements.push(element);
+}
+return elements; }
+
+function f1()
+{
+ a=parseFloat($('tx1').value);
+if(a>0)
+{
+a=a+1;
+}
+
+$('tx2').value=a;
+}
