dc71b2e632e8f445a126ec7175090597a973dd4e
Yuliana29 <39260172+Yuliana29@users.noreply.github.com>
2018-12-06 12:31:50 +0200

    Add files via upload

---
+++ gif.js
@@ -0,0 +1,21 @@
+﻿var t;
+var r1=255;
+var g1=255;
+var b1=255;
+function f()
+{
+var x1=document.getElementById('b1');
+x1.style.left=(parseInt(x1.style.left)+100)+'px';
+x1.style.d=(parseInt(x1.style.top)+100)+'px';
+r1-=100;
+g1-=50;
+b1-=100;
+str1='rgb('+r1+', '+g1+', '+b1+')';
+x1.style.backgroundColor=str1;
+}
+function h()
+{t=setInterval('f()',600);}
+function g()
+{
+clearInterval(t);
+}
