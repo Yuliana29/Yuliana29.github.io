c9b777a7a513a01bb58eee3e0ad09dca3dea5b82
Yuliana29 <39260172+Yuliana29@users.noreply.github.com>
2018-12-13 10:24:55 +0200

    Add files via upload

---
+++ 39.js
@@ -0,0 +1,23 @@
+﻿{ var elements = new Array(); 
+for (var i = 0; i < arguments.length; i++) 
+{ var element = arguments[i]; 
+if (typeof element == 'string') 
+element = document.getElementById(element); 
+if (arguments.length == 1) 
+return element; 
+elements.push(element); 
+} 
+return elements; }
+
+function f1()
+{
+	A=parseFloat($('tx1').value);
+	B=parseFloat($('tx2').value);
+	result='';
+	for(var i=A; i<=B; i++)
+	{
+		for(var j=1; j<= i; j++)
+			result=result+i;
+	}
+	$('tx4').value=result;
+}
